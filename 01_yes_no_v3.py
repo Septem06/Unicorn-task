a181625c5285bd46746ac9779e23691f41b65078
Septem06 <98923493+Septem06@users.noreply.github.com>
2022-03-26 14:24:49 +1300

    initial version of yes/no checker

---
+++ 01_yes_no_v3.py
@@ -0,0 +1,8 @@
+show_instructions = input("have you played this game before? :").lower()
+if show_instructions == "yes" or show_instructions == "y":
+    print("program continues")
+elif show_instructions == "no" or show_instructions == "n":
+    print("display instructions")
+else:
+    print("please answer 'yes' or 'no' ")
+print(f"you entered'{show_instructions}'")
